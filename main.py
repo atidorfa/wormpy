05dec810805ab0fc58a533ba322fb4f264928d6e
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2023-04-10 02:17:32 -0300

    wormpy webscrapper

---
+++ main.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+URL = 'https://www.timesjobs.com/'
+URL_PYTHON = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation='
+TIME_WAIT = 0.1
+
+
+def find_jobs():
+    print('Put some skill that you are not familiar with')
+    unfamiliar_skill = input('>')
+    print(f"Filtering out {unfamiliar_skill}")
+    r = requests.get(URL_PYTHON)
+    # print(r.text)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    # job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f"Company Name: {company_name.strip()}")
+                print(f"Required skills: {skills.strip()}")
+                print(f"More info: {more_info}")
+                print('----------------------------------------')
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        print(f"Waiting {TIME_WAIT} minutes")
+        time.sleep(TIME_WAIT * 60)
